ab7164fbd25ecee45bea66feeaaa4be4d135a7cc
Ibrohim <iqalandarov05@gmail.com>
2024-06-11 11:47:53 +0500

    dd

--- src/Ibrohim.java
+++ src/Ibrohim.java
@@ -1,3 +1,30 @@
 public class Ibrohim {
+String name ;
+String surname;
 
+    public  Ibrohim() {};
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Ibrohim{" +
+               "name='" + name + '\'' +
+               ", surname='" + surname + '\'' +
+               '}';
+    }
 }
